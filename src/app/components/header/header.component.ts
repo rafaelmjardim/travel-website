d531071e876e3076516e9d1a74fa6249908edd7c
rafaelmjardim <rafaelmjardim@hotmail.com>
2022-09-19 10:56:53 -0300

    menu list com ngfor

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+type MenuItem = {
+  name: string,
+  url?: string,
+  class?: string;
+}
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  menuList: MenuItem[] = [
+    {
+    name: 'Services',
+    url: '/services'
+    },
+    {
+    name: 'Destinations',
+    url: '/destinations'
+    },
+    {
+    name: 'Bookings',
+    url: '/bookings'
+    },
+    {
+      name: 'About',
+      url: '/about'
+    },
+    {
+      name: 'Login',
+      url: '/login'
+    },
+    {
+      name: 'Sign Up',
+      url: '/signup',
+      class: 'sign_up_styles'
+    },
+    {
+      name: 'EN',
+    }
+]
 
   constructor() { }
 
