3cc9a16558c3917c916963892c23f434c81dfa50
rafaelmjardim <rafaelmjardim@hotmail.com>
2022-09-19 15:46:10 -0300

    footer finalizado

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,72 @@
 import { Component, OnInit } from '@angular/core';
 
+type FooterItem = {
+  title: string,
+  itemList: FooterItemLink[]
+}
+
+type FooterItemLink = {
+  txt: string
+}
+
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+
+  footerList: FooterItem[] = [
+    {
+      title: 'Company',
+
+      itemList: [
+        {
+          txt: 'About'
+        },
+        {
+          txt: 'Careers'
+        },
+        {
+          txt: 'mobile'
+        }
+      ]
+    
+    },
+    {
+      title: 'Contact',
+
+      itemList: [
+        {
+          txt: 'Help/FAQ'
+        },
+        {
+          txt: 'Press'
+        },
+        {
+          txt: 'Affilates'
+        }
+      ]
+    
+    },
+    {
+      title: 'More',
+
+      itemList: [
+        {
+          txt: 'Airlinefees'
+        },
+        {
+          txt: 'Airline'
+        },
+        {
+          txt: 'Low fare tips'
+        }
+      ]
+    
+    },
+  ]
 
   constructor() { }
 
