54d102b6859c3b26bb6c044099263ec3fe73863c
rafaelmjardim <rafaelmjardim@hotmail.com>
2022-09-20 21:08:35 -0300

    update services ngFor

--- src/app/modules/services/services.component.ts
+++ src/app/modules/services/services.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+type ServiceItem = {
+  img: string,
+  title: string,
+  txt: string
+}
+
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
@@ -7,9 +13,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
+  serviceList!: ServiceItem[];
+    
+
   constructor() { }
 
   ngOnInit(): void {
+    this.serviceList = [
+      {
+        img: './assets/SatelliteIcon.png',
+        title: 'Calculated Weather',
+        txt: "Built Wicket longeradmire do barton vanity itself do in it"
+      },
+      {
+        img: './assets/FlightIcon.png',
+        title: 'Best Flights',
+        txt: "Engrossed listening. Park gate sell they west hard for the."
+      },
+      {
+        img: './assets/VocalIcon.png',
+        title: 'Local Events',
+        txt: 'Barton vanity itself do in it. Preferd to men it engrossed listening.'
+      },
+      {
+        img: './assets/CustomizationIcon.png',
+        title: 'Calculated',
+        txt: 'We deliver outsourced aviation services for military customers'
+      }
+  ] 
+      
+    
   }
 
 }
