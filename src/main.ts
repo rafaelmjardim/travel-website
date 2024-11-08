ba1c105f190b08629af5196e76a1317b9909e2aa
rafaelmjardim <rafaelmjardim@hotmail.com>
2024-11-08 07:59:45 -0300

    adicionado SSR

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app/app-routing.module';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { BrowserModule, bootstrapApplication, provideClientHydration } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
@@ -15,7 +15,7 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
     providers: [
         importProvidersFrom(BrowserModule, AppRoutingModule),
-        provideAnimations()
+        provideAnimations(), provideClientHydration()
     ]
 })
   .catch(err => console.error(err));
